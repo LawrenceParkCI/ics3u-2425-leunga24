6b74b23a7891f3dec1fdb978d8dcc48b64d89e0f
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-09-20 11:11:24 -0400

    Adding ISC3U project wiht HelloWorld Class

---
+++ ICS3U-LeungA/src/unit1/HelloWorld.java
@@ -0,0 +1,20 @@
+package unit1;
+/**
+ * Description: Ths program prints Hello World, and a second statement. <p>
+ * Date: Sept 19, 2024
+ * @author Adam Leung
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("Hello World!");
+		System.out.println("My name is Ms. Kemp");
+		
+		/*
+		 * This is a block comment.
+		 * You can add notes on multiple lines
+		 */
+	}
+
+}
