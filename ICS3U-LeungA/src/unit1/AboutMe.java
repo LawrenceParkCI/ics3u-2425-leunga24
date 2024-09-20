3223c0aa5343271c0bfc75235be89cad2d56d45d
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-09-20 11:41:50 -0400

    A little bit about me

---
+++ ICS3U-LeungA/src/unit1/AboutMe.java
@@ -0,0 +1,18 @@
+package unit1;
+/**
+ * Description: This program prints information about the author, Adam Leung
+ * Date: Sept 20, 2024
+ * Authour: @authour Adam Leung
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// These following statements give information about who I am
+		System.out.println("Hi, my name is Adam Leung");
+		System.out.println("I am a 15 year old boy in grade 11");
+		System.out.println("My teacher is awesome her name is Ms. Kemp");
+		System.out.println("I got to Lawrence Park Collegiate Institute");
+	}
+	
+
+}
