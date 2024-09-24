1332df2e3770c0b7532c8a899ec1cce506b43a68
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-09-24 11:41:33 -0400

    worked on timetable

--- ICS3U-LeungA/src/unit1/AboutMe.java
+++ ICS3U-LeungA/src/unit1/AboutMe.java
@@ -10,9 +10,16 @@ public class AboutMe {
 		// These following statements give information about who I am
 		System.out.println("Hi, my name is Adam Leung");
 		System.out.println("I am a 15 year old boy in grade 11");
-		System.out.println("My teacher is awesome her name is Ms. Kemp");
-		System.out.println("I got to Lawrence Park Collegiate Institute");
+		System.out.println("I was born on September 25th, 2008");
+		System.out.println("My teacher is awesome and her name is Ms. Kemp");
+		System.out.println("I go to Lawrence Park Collegiate Institute in Toronto, Canada");
+		System.out.println("\"GO LPCI PANTHERS!");
+		System.out.println("------------------------------------------------------------------");
+		// The next several statements display my course schedule for the week of September 23rd
+		System.out.println("My course schedule for the week of September 23rd");
+		System.out.println("\n__________________________________________________________________________________");
+		System.out.printf("%-19s%-20s%-20s%-20s%-20s", "Monday (September 23)", "Tuesday (September 24)", "Wednesday (September 25 *My B-Day*)", "Thursday (September 26)", "Friday (September 27)");
+		System.out.println("\n__________________________________________________________________________________");
+		System.out.printf("%-19s%-20s%-20s%-20s%-20s", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
 	}
-	
-
 }
