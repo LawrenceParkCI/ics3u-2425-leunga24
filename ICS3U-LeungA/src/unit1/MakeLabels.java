bb586146703ae5556dc468bffb4115cfb3ef0e73
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-10-02 11:18:13 -0400

    Finished Make Labels assignment

--- ICS3U-LeungA/src/unit1/MakeLabels.java
+++ ICS3U-LeungA/src/unit1/MakeLabels.java
@@ -1,22 +1,35 @@
 package unit1;
-
+/**
+ * Description: This program creates variables
+ * Date: October 2, 2024
+ * @author Adam Leung
+ */
 import java.util.Scanner;
 
 public class MakeLabels {
-
+	/**
+	 * This is the entry point to the program.
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner (System.in);
-		
+
+
 		String subject;
-		
+		String name;
+		String last;
 		
 		System.out.println("Type in the subject and press <Enter>");
-		subject = sc.next();
+		subject = sc.nextLine();
+
+		System.out.println("Type in the name and press <Enter>");
+		name = sc.nextLine();
+	
 		
 		System.out.println();
 		System.out.println("******************************");
-		System.out.println("Tenzin");
+		System.out.println(name);
 		System.out.println(subject);
 		System.out.println("******************************");
 		sc.close();
