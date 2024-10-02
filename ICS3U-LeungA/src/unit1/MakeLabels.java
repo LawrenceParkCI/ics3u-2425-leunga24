90c610168cd7e6116d2214b72d3053f3884592cf
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-10-02 10:53:53 -0400

    Finished Part A (4.)

--- ICS3U-LeungA/src/unit1/MakeLabels.java
+++ ICS3U-LeungA/src/unit1/MakeLabels.java
@@ -7,10 +7,17 @@ public class MakeLabels {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner (System.in);
+		
+		String subject;
+		
+		
+		System.out.println("Type in the subject and press <Enter>");
+		subject = sc.next();
+		
 		System.out.println();
 		System.out.println("******************************");
-		System.out.println("John");
-		System.out.println("Computers");
+		System.out.println("Tenzin");
+		System.out.println(subject);
 		System.out.println("******************************");
 		sc.close();
 	}
