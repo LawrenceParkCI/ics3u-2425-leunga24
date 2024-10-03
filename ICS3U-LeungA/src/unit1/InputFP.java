6e36bc060c22036161c05326cdc3b171947b1195
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-10-03 09:38:16 -0400

    Removed TODO comments and finished InputFP worksheet

---
+++ ICS3U-LeungA/src/unit1/InputFP.java
@@ -0,0 +1,34 @@
+package unit1;
+
+import java.util.Scanner;
+
+/**
+ * Description: This program 
+ * Date: October 3, 2024
+ * @author Adam Leung
+ */
+public class InputFP {
+	/**
+	 * This is the entry point to the program.
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		double num1, num2;
+		// Receives user input
+		System.out.println("Floating-Point");
+		System.out.print("Type in the number and <Enter>: ");
+		num1 = sc.nextDouble();
+		
+		System.out.print("Type in the number and <Enter>: ");
+		num2 = sc.nextDouble();
+		
+		//Print the output
+		System.out.println();
+		System.out.println("The first number entered was " + num1);
+		System.out.println("The second number entered was " + num2);
+
+	}
+
+}
