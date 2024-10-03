e5950762e94df3ee133203031e3af30a75b7907d
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-10-03 10:08:29 -0400

    Finished InputChars Worksheet

--- ICS3U-LeungA/src/unit1/InputChars.java
+++ ICS3U-LeungA/src/unit1/InputChars.java
@@ -1,7 +1,9 @@
 package unit1;
+
+import java.util.Scanner;
 /**
- * Description: This program prints information about the author, Adam Leung
- * Date: September 20, 2024
+ * Description: This program teaches the programmer about Inputing Characters
+ * Date: October 3, 2024
  * @author Adam Leung
  */
 public class InputChars {
@@ -10,8 +12,18 @@ public class InputChars {
 	 * @param args unused
 	 */
 	public static void main(String[] args) {
-		
 
+		Scanner sc = new Scanner(System.in);
+
+		char ch1, ch2, ch3;
+		System.out.println("Type in any three characters on the keyboard");
+		System.out.println("Press <Enter> after each.");
+		ch1 = sc.nextLine().charAt(0);
+		ch2 = sc.nextLine().charAt(0);
+		ch3 = sc.nextLine().charAt(0);
+		System.out.println();
+		System.out.println("Together these 3 letters spell: " + (ch1 + ch2 + ch3));
+		sc.close();
 	}
 
 }
