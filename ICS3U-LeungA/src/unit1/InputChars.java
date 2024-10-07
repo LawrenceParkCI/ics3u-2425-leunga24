713de3cc9dd8584ce11df6c14c6f602835f6ef0b
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-10-07 09:20:42 -0400

    Pushing PlayingWithVariables

--- ICS3U-LeungA/src/unit1/InputChars.java
+++ ICS3U-LeungA/src/unit1/InputChars.java
@@ -14,14 +14,16 @@ public class InputChars {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
+		//Declare variables
 		char ch1, ch2, ch3;
+		//get user input
 		System.out.println("Type in any three characters on the keyboard");
 		System.out.println("Press <Enter> after each.");
 		ch1 = sc.nextLine().charAt(0);
 		ch2 = sc.nextLine().charAt(0);
 		ch3 = sc.nextLine().charAt(0);
 		System.out.println();
+		//Calculate and prints output
 		System.out.println("Together these 3 letters spell: " + (ch1 + ch2 + ch3));
 		sc.close();
 	}
--- ICS3U-LeungA/src/unit1/PlayingWithStrings.java
+++ ICS3U-LeungA/src/unit1/PlayingWithStrings.java
@@ -1,10 +1,15 @@
 package unit1;
-
+/**
+ * Description: This program 
+ * Date: October 4, 2024
+ * @author Adam Leung
+ */
 public class PlayingWithStrings {
-
+	/**
+	 * This is the entry point to the program.
+	 * @param args unused
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
-}
+	
+	}}
