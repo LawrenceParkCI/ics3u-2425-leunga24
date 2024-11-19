8a850a0aea137e3a3ad0e39465d033130dd07647
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2024-11-19 09:53:01 -0500

    Finished IfChallenge 3! Longest code yet! :)

---
+++ ICS3U-LeungA/src/unit2/Randomness.java
@@ -0,0 +1,27 @@
+package unit2;
+/**
+
+https://www.geeksforgeeks.org/java-math-random-method-examples/
+
+Description: This program randomly generates numbers
+Date: November 19, 2024
+@author: Adam Leung
+*/
+public class Randomness {
+	/** 
+	 * This is the entry point to the program.
+	 * @param args unused
+	 */
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println(Math.random());
+    }
+
+    //Run the program a few times. What do you think is the 
+    //range of the numbers generated?
+
+    //Read the demo program. Write the code so that the program 
+    //will produce a random number between 100 and 1000.
+    
+  }
+}
