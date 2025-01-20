782bf4100af9fe8f3f8d3353a5fe6b0d9c48fc53
342974995 <342974995@C-MXL2393F1P.tdsb.on.ca>
2025-01-20 10:22:03 -0500

    done

---
+++ ICS3U-LeungA/src/unit3/MathPlus.java
@@ -0,0 +1,18 @@
+package unit3;
+/**
+Description:
+Date: January 12, 2025
+@author Adam Leung
+ */
+public class MathPlus {
+	/** 
+	 * This is the entry point to the program.
+	 * @param args unused
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
